e13b1454edb72fdd877bf6580a2d67f63869397b
Benjamin Yuly <ben.yuly@gmail.com>
2020-10-06 15:03:06 -0400

    Created example controller.

--- src/main/java/com/benjaminyuly/harbinger/RandomizeController.java
+++ src/main/java/com/benjaminyuly/harbinger/RandomizeController.java
@@ -1,9 +1,18 @@
 package com.benjaminyuly.harbinger;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RandomizeController {
+	
+	@GetMapping("/randomize")
+	public String RandomizeGame(@RequestParam(required = false)String expansion, Model model) {
+		model.addAttribute("expansion", expansion);
+		return "randomized";
+	}
 	
 
 }
